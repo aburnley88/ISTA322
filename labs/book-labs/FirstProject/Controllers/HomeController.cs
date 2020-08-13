ec41d53b9aa2ae007a4f567ab1b7f739043171a5
aBurnley <aburnleylvx@outlook.com>
2020-08-13 10:37:30 -0400

    adding 1st HTML lab and 1st ASP.Net lab

---
+++ labs/book-labs/FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView", viewModel);
+        }
+    }
+}
+
+        //private readonly ILogger<HomeController> _logger;
+
+//        public HomeController(ILogger<HomeController> logger)
+//        {
+//            _logger = logger;
+//        }
+
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+
+//        public IActionResult Privacy()
+//        {
+//            return View();
+//        }
+
+//        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//        public IActionResult Error()
+//        {
+//            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//        }
+//    }
+//}
